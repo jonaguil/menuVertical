037347e24c269048b0fc7fdf662bfa05ca66ed35
jonaguil <kudabt@gmail.com>
2023-11-16 09:38:27 -0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,4 @@
+function toggleMenu() {
+  const mobileMenu = document.querySelector('.mobile-menu');
+  mobileMenu.style.display === 'block' ? mobileMenu.style.display = 'none' : mobileMenu.style.display = 'block';
+}
